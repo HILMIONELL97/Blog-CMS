20999338308acad4ac7938bf0340c5bb866ba365
hilmimohammed97@gmail.com <simo.1997.gov@gmail.com>
2020-07-15 02:29:38 +0100

    Complete PHP blog code for show.php file

---
+++ blog/header.php
@@ -0,0 +1,7 @@
+<link href="http://localhost/blog/assets/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<ul class="ulclass">
+<li><a href="http://localhost/blog/">Home>/a<</li>
+
+</ul>
